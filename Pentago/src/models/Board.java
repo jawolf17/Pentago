16c50bb519f208ba5619c081e6f965283cb30ec6
jpcerone <jpcerone@gmail.com>
2015-11-11 11:14:02 -0500

    Added Player, Game, and Controller classes

--- Pentago/src/models/Board.java
+++ Pentago/src/models/Board.java
@@ -1,8 +1,31 @@
 package models;
 
 public class Board {
+	int[][] bigBoard;
+	int[][] a;
+	int[][] b;
+	int[][] c;
+	int[][] d;
+	
 	public Board() {
-		String joe = "badass";
+		bigBoard = int[8][8];
+		a = int[3][3];
+		b = int[3][3];
+		c = int[3][3];
+		d = int[3][3];
+		
+	}
+	public boolean place(int row, int col,player p){
+		return false;
+	}
+	public void rotate(char q,boolean d){
+		
+	}
+	public boolean isOccupied(int row, int col){
+		return false;
+	}
+	public boolean isWon(){
+		return false;
 	}
 	
 
---
+++ Pentago/src/models/Player.java
@@ -0,0 +1,15 @@
+
+public class Player {
+	String color;
+	String name;
+	boolean isTurn;
+	public Player(String c,String n){
+		color = c;
+		name = n;
+		isTurn = false;
+	}
+	public void turn(){
+		isTurn = true;
+		
+	}
+}
