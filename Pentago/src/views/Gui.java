a060992c3a0da90750a69049b4efb3181c2af289
jpcerone <jpcerone@gmail.com>
2015-11-11 20:35:14 -0500

    GUI started, main menu complete

---
+++ Pentago/src/views/Gui.java
@@ -0,0 +1,54 @@
+package views;
+
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.GeneralPath;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import models.Controller;
+
+public class Gui extends Frame{
+		private Controller con;
+		public static void main(String[] args){
+			new Gui();
+		}
+		public Gui(/*Controller c*/) {
+			     //Title our frame.
+			    super("Pentago");
+			    /*con = c;*/
+			    setSize(400,300);
+			    setVisible(true);
+			    addWindowListener(new WindowAdapter()
+			    		      {public void windowClosing(WindowEvent e)
+			    		          {dispose(); System.exit(0);}
+			    		       }
+			    		     );
+		}
+		public void paint(Graphics g) {
+			/*g.setColor(Color.red);
+			g.drawRect(50,50,200,200);
+			Graphics2D g2d = (Graphics2D)g;
+			g2d.setColor(Color.blue);
+			g2d.drawRect(75,75,300,200);*/
+			JTextField onePlayer = new JTextField("One Player");
+			JTextField twoPlayer = new JTextField("Two Players");
+			onePlayer.addActionListener(new ActionListener() { 
+			    public void actionPerformed(ActionEvent e) { 
+			    	
+			    } 
+			});
+			twoPlayer.addActionListener(new ActionListener() { 
+			    public void actionPerformed(ActionEvent e) { 
+			        
+			    } 
+			});
+		}
+}
---
+++ Pentago/src/views/Menu.java
@@ -0,0 +1,84 @@
+package views;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+import models.Controller;
+
+import java.awt.event.ActionEvent;
+
+public class Menu {
+	private Controller con;
+	public Menu(Controller c){
+	  con = c;
+      ButtonFrame frame = new ButtonFrame(con);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+   }
+}
+
+class ButtonFrame extends JFrame {
+
+   private JTextField PlayerOne;// !! Make field private
+   private JTextField PlayerTwo;
+   private JRadioButton cpu;
+   @SuppressWarnings("LeakingThisInConstructor")
+   public ButtonFrame(Controller con) {
+
+      setTitle("Pentago");
+      setSize(900, 900);
+      ButtonPanel panel = new ButtonPanel(con);
+      panel.add(new JLabel("Player 1:"));
+      panel.add(new JLabel("Player 2:"));
+      PlayerOne = new JTextField(40);
+      PlayerTwo = new JTextField(40);
+      cpu = new JRadioButton("CPU");
+      panel.add(PlayerOne);
+      panel.add(PlayerTwo);
+      panel.add(cpu);
+      cpu.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	PlayerTwo.setText("CPU");
+            }
+      });
+      add(panel, BorderLayout.CENTER);
+   }
+
+   // !! create a public method to get JTextField's text
+   // !! without exposing the JTextField itself.
+   public String getPlayerOneName() {
+      return PlayerOne.getText();
+   }
+   public String getPlayerTwoName(){
+	   return PlayerTwo.getText();
+   }
+
+   class ButtonPanel extends JPanel implements ActionListener {
+      private Component frame;
+
+      public ButtonPanel(Controller con) {
+
+         final JButton b2 = new JButton("Submit");
+         add(b2, BorderLayout.SOUTH);
+         b2.setActionCommand("Submit");
+         b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               // !! ButtonFrame bf = new ButtonFrame();
+               if ("Submit".equals(e.getActionCommand())) {
+            	   con.createGame(getPlayerOneName(),getPlayerTwoName());
+                  
+               }
+            }
+
+         });
+
+      }
+
+      @Override
+      public void actionPerformed(ActionEvent ae) {
+         throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+   }
+}
