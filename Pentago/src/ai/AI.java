24b88e5955896062250aba184baa269bdd1fa953
jawolf17 <jawolf@buffalo.edu>
2015-11-16 20:08:47 -0500

    Overall decision process made for AI

--- Pentago/src/ai/AI.java
+++ Pentago/src/ai/AI.java
@@ -8,7 +8,11 @@ package ai;
 
 import models.Board;
 import models.Player;
+
+import java.util.Random;
+
 import controller.Controller;
+import util.Maneuver;
 
 public class AI extends Player {
 
@@ -43,19 +47,78 @@ public class AI extends Player {
 		}
 	
 	private void turnEasy(){
-		int row=0;
-		int col =0;
-		while(row<6&&col<6&&!getPlaced()){
-			if(_test_board.checkPlacement(row,col)){
-				_con.place(row, col, getColor());
-				_con.dummyRotate();
-				
+		Boolean turnTaken = false;
+		Maneuver win_result = _test_board.canWin();
+		if(win_result.column!=-1&&win_result.column!=-1){
+			_con.place(win_result.row,win_result.column,getColor());
+			if(win_result.quad!='z'){
+				 _con.rotate(win_result.quad, win_result.dir);
 			}
-			col++;
-			if(col==6){
-				col=0;
-				row++;
+			else{
+				_con.dummyRotate();
+			}
+			turnTaken=true;
+		}
+		if(!turnTaken){
+			Maneuver loss_prevent = _test_board.canLose();
+			if(loss_prevent.row!=-1&&loss_prevent.column!=-1){
+				_con.place(loss_prevent.row, loss_prevent.column, getColor());
+				if(loss_prevent.quad !='z'){
+					_con.rotate( loss_prevent.quad, loss_prevent.dir);
+				}
+				else{
+					_con.dummyRotate();
+				}
+				turnTaken = true;
+			}
+		}
+		if(!turnTaken){
+			Random r = new Random();
+			while(!turnTaken){
+				int row = r.nextInt(6);
+				int col = r.nextInt(6);
+				if(!_board_actual.isOccupied(row, col)){
+					_con.place(row, col, getColor());
+					turnTaken=true;
+				}
+				if(!_board_actual.isNeutral()){
+					int quad_int = r.nextInt(4);
+					int quadr_dir = r.nextInt(2);
+					char quad_char='a';
+					switch(quad_int){
+						case 0: quad_char = 'a';
+						break;
+						case 1: quad_char = 'b';
+						break;
+						case 2: quad_char = 'c';
+						break;
+						case 3: quad_char = 'd';
+						break;
+					}
+						if(quadr_dir==0){
+							_con.rotate(quad_char, false);
+						}
+						else{
+							_con.rotate(quad_char,true);
+						}
+				}
 			}
 		}
 	}
+	
+//		int row=0;
+//		int col =0;
+//		while(row<6&&col<6&&!getPlaced()){
+//			if(_test_board.checkPlacement(row,col)){
+//				_con.place(row, col, getColor());
+//				_con.dummyRotate();
+//				
+//			}
+//			col++;
+//			if(col==6){
+//				col=0;
+//				row++;
+//			}
+//		}
 }
+
