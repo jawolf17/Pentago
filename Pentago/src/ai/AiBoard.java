85cb6d3e86137ffac62384a18e779eb39e18e8ea
jpcerone <jpcerone@gmail.com>
2015-11-16 19:37:56 -0500

    AiBoard revamp

--- Pentago/src/ai/AiBoard.java
+++ Pentago/src/ai/AiBoard.java
@@ -12,16 +12,296 @@ import controller.Controller;
 import models.Board;
 import util.Maneuver;
 
-public class AiBoard extends Board {
+public class AiBoard{
 
-	private Board _cur_board;
+	private int[][] bigBoard;
+	private int[][] a;
+	private int[][] b;
+	private int[][] c;
+	private int[][] d;
+	private Controller control;
+	private boolean neutral_a;
+	private boolean neutral_b;
+	private boolean neutral_c;
+	private boolean neutral_d;
 	
 	
 	public AiBoard(Board curBoard,Controller controller) {
-		super(controller);
-		_cur_board = curBoard;
+		control = controller;
+		bigBoard = curBoard.getBigBoard();
+		a = curBoard.getQuadA();
+		b = curBoard.getQuadB();
+		c = curBoard.getQuadC();
+		d = curBoard.getQuadD();
 	}
-	
+	/**
+	 * Used to place pieces on the board.
+	 * @param int row, int col, int p(0,1,or 2 1 representing white, 2 representing black)
+	 * @return boolean
+	 */
+	public boolean place(int row, int col,int p){
+		if((row > 5 || col > 5) || (p<=0 || p>3)|| isOccupied(row,col)){
+			return false;	
+		}
+		if(row <= 2 && col <= 2){
+			if(row!=1&&col!=1&&neutral_a){
+				neutral_a=false;
+			}
+			a[row][col] = p;
+			update('a');
+		}
+		else if(row <= 2 && col >= 3){
+			if(row!=1&&col!=4&&neutral_b){
+				neutral_b=false;
+			}
+			b[row][col-3] = p;
+			update('b');
+		}
+		else if(row >=3 && col <= 2){
+			if(row!=4&&col!=1&&neutral_c){
+				neutral_c=false;
+			}
+			c[row-3][col] = p;
+			update('c');
+		}
+		else if(row >=3 && col >= 3){
+			if(row!=4&&col!=4&&neutral_d){
+				neutral_d=false;
+			}
+			d[row-3][col-3] = p;
+			update('d');
+		}
+		return true;
+	}
+	/**
+	 * rotate is used to rotate one quadrant a quarter turn.
+	 * 
+	 * @param char q(representing quadrant) boolean r(true is ccw, false is cw)
+	 * @return none
+	 */
+	public void rotate(char q,boolean r){
+		if(q=='a'){
+			if(r == false){
+				a = clockwise(a);
+			}
+			else if(r == true){
+				a = counterclockwise(a);
+			}
+			update('a');
+		}
+		else if(q =='b'){
+			if(r == false){
+				b = clockwise(b);
+			}
+			else if(r == true){
+				b = counterclockwise(b);
+			}
+			update('b');
+		}
+		else if(q == 'c'){
+			if(r == false){
+				c = clockwise(c);
+			}
+			else if(r == true){
+				c = counterclockwise(c);
+			}
+			update('c');
+		}
+		else if(q == 'd'){
+			if(r == false){
+				d = clockwise(d);
+			}
+			else if(r == true){
+				d = counterclockwise(d);
+			}
+			update('d');
+		}
+	}
+	//hard coded for now, didnt bother figuring out math behind it
+	private int[][] clockwise(int[][] quad){
+		int[][] temp = new int[3][3];
+		temp[0][0] = quad[2][0];
+		temp[0][1] = quad[1][0];
+		temp[0][2] = quad[0][0];
+		temp[1][0] = quad[2][1];
+		temp[1][1] = quad[1][1];
+		temp[1][2] = quad[0][1];
+		temp[2][0] = quad[2][2];
+		temp[2][1] = quad[1][2];
+		temp[2][2] = quad[0][2];
+		return temp;
+	}
+	private int[][] counterclockwise(int[][] quad){
+		int[][] temp = new int[3][3];
+		temp[2][0] = quad[0][0];
+		temp[1][0] = quad[0][1];
+		temp[0][0] = quad[0][2];
+		temp[2][1] = quad[1][0];
+		temp[1][1] = quad[1][1];
+		temp[0][1] = quad[1][2];
+		temp[2][2] = quad[2][0];
+		temp[1][2] = quad[2][1];
+		temp[0][2] = quad[2][2];
+		return temp;
+		
+	}
+	/**
+	 * isOccupied returns true if the space given has a token in it already.
+	 * 
+	 * @param int row int col
+	 * @return boolean
+	 */
+	public boolean isOccupied(int row, int col){
+		if(bigBoard[row][col] == 0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	public boolean isNeutral(){
+		
+		return neutral_a || neutral_b || neutral_c || neutral_d;
+	}
+	/**
+	 * isWon returns true is there is a winning grouping of 5 on the board.
+	 * 
+	 * @param none
+	 * @return boolean
+	 */
+	public int isWon(){
+		int org = 0;
+		for(int i=0;i<bigBoard[0].length;i++){
+			for(int j=0;j<bigBoard.length;j++){
+				if(bigBoard[i][j] == 1 || bigBoard[i][j] == 2){
+					org = bigBoard[i][j];
+					int next = i+1;
+					int next3 = j+1;
+					int next4 = j-1;
+					if(next3<6 && bigBoard[i][next3] == org){
+						next3 = next3 +1;
+						if(next3<6 && bigBoard[i][next3] == org){
+							next3 = next3 +1;
+							if(next3<6 && bigBoard[i][next3] == org){
+								next3 = next3 +1;
+								if(next3<6 && bigBoard[i][next3] == org){
+									return org;
+								}
+							}
+						}
+					}
+					if(next<6 && bigBoard[next][j] == org){
+						next = next+1;
+						if(next<6 && bigBoard[next][j] == org){
+							next = next+1;
+							if(next<6 && bigBoard[next][j] == org){
+								next = next+1;
+								if(next<6 && bigBoard[next][j] == org){
+									return org;
+								}
+							}
+						}
+					}
+					next = i+1;
+					next3 = j+1;
+					next4 = j-1;
+					if((next3<6 && next <6) && bigBoard[next][next3] == org){
+						next3 = next3 +1;
+						next = next +1;
+						if((next3<6 && next <6) && bigBoard[next][next3] == org){
+							next3 = next3 +1;
+							next = next +1;
+							if((next3<6 && next <6) && bigBoard[next][next3] == org){
+								next3 = next3 +1;
+								next = next +1;
+								if((next3<6 && next <6) && bigBoard[next][next3] == org){
+									return org;
+								}
+							}
+						}
+					}
+					next = i+1;
+					if((next4>0 && next <6) && bigBoard[next][next4] == org){
+						next4 = next4 -1;
+						next = next +1;
+						if((next4>0 && next <6) && bigBoard[next][next4] == org){
+							next4 = next4 -1;
+							next = next +1;
+							if((next4>0 && next <6) && bigBoard[next][next4] == org){
+								next4 = next4 -1;
+								next = next +1;
+								if((next4>0 && next <6) && bigBoard[next][next4] == org){
+									return org;
+								}
+							}
+						}
+					}
+					
+					
+				
+					
+				}
+			}
+		}
+		return org;
+	}
+	private void update(){
+		for(int i = 0;i<a[0].length;i++){
+			for(int j = 0;j<a.length;j++){
+				bigBoard[i][j] = a[i][j];
+			}
+		}
+		for(int k=0;k<b[0].length;k++){
+			for(int l = 0;l<b.length;l++){
+				bigBoard[k][l+3] = b[k][l];
+			}
+		}
+		for(int q=0;q<c[0].length;q++){
+			for(int w = 0;w<c.length;w++){
+				bigBoard[q+3][w] = b[q][w];
+			}
+		}
+		for(int e=0;e<d[0].length;e++){
+			for(int r = 0;r<d.length;r++){
+				bigBoard[e+3][r+3] = b[e][r];
+			}
+		}
+		control.update();
+		
+	}
+	private void update(char q){
+		switch(q){
+			case 'a':
+				for(int i = 0;i<a[0].length;i++){
+					for(int j = 0;j<a.length;j++){
+						bigBoard[i][j] = a[i][j];
+					}
+				}
+				break;
+			case 'b':
+				for(int k=0;k<b[0].length;k++){
+					for(int l = 0;l<b.length;l++){
+						bigBoard[k][l+3] = b[k][l];
+					}
+				}
+				break;
+			case 'c':
+				for(int u=0;u<c[0].length;u++){
+					for(int w = 0;w<c.length;w++){
+						bigBoard[u+3][w] = c[u][w];
+					}
+				}
+				break;
+			case 'd':
+				for(int e=0;e<d[0].length;e++){
+					for(int r = 0;r<d.length;r++){
+						bigBoard[e+3][r+3] = d[e][r];
+					}
+				}
+				break;
+		}
+		control.update();
+	}
 	/**
 	 * Checks to see if position at row column will result in "preferred placement" (define that later)
 	 * Intended to check all possible scenarios of a position
@@ -33,13 +313,21 @@ public class AiBoard extends Board {
 		return !isOccupied(row,col);
 	}
 	 public Maneuver canLose(){
+		 int op;
+		 if(control.getCurrentPlayer().getColor() == 1){
+			 op = 2;
+		 }
+		 else{
+			 op = 1;
+		 }
 		 Maneuver p = new Maneuver();
-		 int[][] big = _cur_board.getBigBoard();
+		 int[][] big = bigBoard;
 		 for(int i = 0;i<big[0].length;i++){
 			 for(int j = 0;j<big.length;j++){
-				 if(!_cur_board.isOccupied(i,j)){
-					 
+				 if(!isOccupied(i,j)){
+					 place(i,j,op);
 				 }
+				 dummyUpdate();
 			 }
 		 }
 		 return p;
@@ -53,12 +341,7 @@ public class AiBoard extends Board {
 	  * Updates the AIBoard to match the current state of the game Board
 	  */
 	 public void dummyUpdate(){
-	     ArrayList<int[][]> copies = _cur_board.getDeepCopies();
-	     setA(copies.get(0));
-	     setB(copies.get(1));
-	     setC(copies.get(2));
-	     setD(copies.get(3));
-	     setBigBoard(copies.get(4));	     
+		 
 	 }		
 	
 
